a24c76a7111c4e28e7b1a13b0145f764f01e4afe
Adam Kariv <adam.kariv@gmail.com>
2018-04-30 17:24:08 +0300

    First version of main page

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,63 @@ import {Component, ViewChild} from '@angular/core';
 @Component({
   selector: 'my-app',
   template: ` 
-      <budgetkey-container [showHeader]="true" [showSearchBar]="true">
-        <div class="container-fluid scroll" style="position: relative">
-          <h1>Hello Social Map!</h1>
+      <budgetkey-container [showHeader]="true" [showSearchBar]="false">
+        <div class='background-graphic'>
+          <img src="assets/img/bg1.jpg"/>
+        </div>
+        <div class="main-text">
+          <h1>אז מה באמת קורה במגזר השלישי!?</h1>
+          <input class="input" type="text"/>
+          <div class="search-guide">
+            <img src="assets/img/down-left-arrow.png"/>
+            <div class="search-examples">
+              <div>חיפושים לדוגמה</div>
+              <div class="tags">
+                <span class="tag">ארגונים פעילים בתחום התרבות והאמנות</span>
+                <span class="tag">ארגונים הפעילים במחוז הדרום</span>
+                <span class="tag">״המרכז לעיוור בישראל״</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="tab-buttons">
+          <div class="tab-button" (click)="active='flag'"
+               [ngClass]="{active: active=='flag'}"
+          >
+            <img class="tab-icon" src="assets/img/icon-flag.svg"/> 
+            <div class="text">
+              ארגונים אלו מדווחים על פעילות ב-<strong>{{ data.details.total_active_cities }}</strong> ישובים בשלוש השנים האחרונות 
+            </div>
+          </div>
+          <div class="tab-button" (click)="active='hadash'"
+               [ngClass]="{active: active=='hadash'}"
+          >
+            <img class="tab-icon" src="assets/img/icon-hadash.svg"/> 
+            <div class="text">
+              <strong>{{ data.details.total_active_orgs }}</strong>
+               ארגונים פעילים בישראל ב-{{ data.details.total_active_foas }}
+               תחומים בשלוש השנים האחרונות
+            </div>
+          </div>
+          <div class="tab-button" (click)="active='coins'"
+               [ngClass]="{active: active=='coins'}"
+          >
+            <img class="tab-icon" src="assets/img/icon-flag.svg"/> 
+            <div class="text">
+              בשלוש השנים האחרונות דווחו 
+              <strong>{{ data.details.total_received.toLocaleString('en-US', {style: 'decimal', maximumFractionDigits: 2}) }} ₪</strong>
+               העברות מכספי ממשלה לארגונים            
+            </div>
+          </div>        
         </div>
       </budgetkey-container>
   `,
 })
 export class AppComponent {
+
+  private active: string = 'hadash';
+  private data: any = window['prefetchedData'];
+
   constructor() {
   }
 }
---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const nunjucks = require('nunjucks');
+const request = require("request");
+const urlencode = require('urlencode');
+
+const basePath = process.env.BASE_PATH || '/';
+const rootPath = path.resolve(__dirname, './dist');
+const disableCache = process.env.DISABLE_CACHE || false;
+
+const app = express();
+if (disableCache) {
+  app.disable('view cache');
+}
+
+nunjucks.configure(rootPath, {
+  autoescape: true,
+  noCache: disableCache,
+  express: app
+});
+
+app.set('port', process.env.PORT || 8000);
+
+app.get(basePath + '*', function(req, res) {
+  var filePath = path.resolve(rootPath, req.params[0]);
+  if (fs.existsSync(filePath)) {
+    var stats = fs.lstatSync(filePath);
+    if (stats.isFile()) {
+      return res.sendFile(filePath);
+    }
+  }
+
+  let doc_id = 'reports/ngos-main-page';
+  request({
+    url: 'https://next.obudget.org/get/' + urlencode(doc_id),
+    json: true
+  }, function (error, response, body) {
+    if (response.statusCode === 200 && body !== null && body.value) {
+      body = body.value;
+      res.render('index.html', {
+        base: basePath,
+        prefetchedData: JSON.stringify(body),
+      });
+    } else {
+      res.sendStatus(response.statusCode);
+    }
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Listening port ' + app.get('port'));
+});
