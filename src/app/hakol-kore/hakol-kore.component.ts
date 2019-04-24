8316ad826ff431979065e159bb55ade193adb8a3
Adam Kariv <adam.kariv@gmail.com>
2019-04-24 23:53:43 +0300

    Hakol kore page

--- src/app/hakol-kore/hakol-kore.component.ts
+++ src/app/hakol-kore/hakol-kore.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HakolKoreComponent implements OnInit {
 
+  searchBarConfig = [
+    {
+      'name': 'הזדמנויות',
+      'id': 'opportunities',
+      'description': 'הזדמנויות במגזר השלישי? כאן תמצאו קולות קוראים, מכרזים, מבחני תמיכה ועוד!',
+      'types': ['tenders', 'calls_for_bids', 'support_criteria'],
+      'placeholder': 'הקלידו מילות חיפוש כמו תחום פעילות, נושא או מונח רלוונטי'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onNavigate(url: string) {
+    window.location.href = url;
   }
 
 }
